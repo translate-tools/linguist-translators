efb95ad44bba024bfb933fde2402888e48c684a8
Robert Vitonsky <robert@vitonsky.net>
2025-09-09 09:15:01 +0200

    chore: add ChatGPT translator

---
+++ src/translators/ChatGPT.ts
@@ -0,0 +1,21 @@
+import { ChatGPTLLMTranslator as BaseChatGPTTranslator } from 'anylang/translators';
+
+type Options = typeof BaseChatGPTTranslator extends { new(...params: infer T): any } ? T : never;
+
+function ChatGPTTranslator(options: Partial<Options>) {
+	return new BaseChatGPTTranslator({
+		...options,
+		// Insert your API key here
+		apiKey: '',
+
+		// Optional. Custom API endpoint
+		// apiOrigin: 'https://openrouter.ai/api/v1',
+
+		// Optional. Custom model name
+		// model: 'openai/gpt-4o-mini',
+	});
+}
+
+ChatGPTTranslator.__proto__ = BaseChatGPTTranslator.translatorName;
+
+ChatGPTTranslator;
